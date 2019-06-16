d0a9f884cc26683537ad891544ba5c05c093b6a7
Ailton Muniz Santos Junior <ailton.msj@gmail.com>
2019-06-16 20:15:48 -0300

    Added script to controll the status from process by file

---
+++ sleepwalkingserver_controll.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# ro run ./sleepwalkingserver_controll.sh start
+# ro run ./sleepwalkingserver_controll.sh stop
+
+case "$1" in
+"start" | "START")
+    ./sleep-walking-server &
+    ;;
+"stop" | "STOP")
+    kill $(cat /tmp/sleepwalkingserver.pid)
+    ;;
+*)
+    echo "Usage $0 start | stop";
+    exit 1;
+esac
