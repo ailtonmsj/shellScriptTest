946eb5036b924a3b4eee88e22001add26dfe82d2
Ailton Muniz Santos Junior <ailton.msj@gmail.com>
2019-06-16 21:30:09 -0300

    Added a tag for log

--- simple_logger.sh
+++ simple_logger.sh
@@ -7,5 +7,6 @@ RANDOM=$RANDON
 # -i for get the PID
 # -s for echo the log on console
 # -p to pass the facilities and severity (for this example user.info)
+# -t for tag the log
 
-logger -s -i -p user.info "THE RANDON NUMBER IS ${RANDON}"
+logger -t testScript -s -i -p user.info "THE RANDON NUMBER IS ${RANDON}"
